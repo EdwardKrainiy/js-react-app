21c99021230bca19eeaa20eb7718f5ad7552f090
Edvard Krainiy <ekrayniy@inbox.ru>
2022-02-10 16:26:50 +0300

    Additional page was created.

---
+++ src/helloworld-page.tsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello world!</h2>
+        <h3>How are you?</h3>
+      </div>
+    );
+  }
+}
